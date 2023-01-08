701a5e3f37142a92a4d33651cc91dcb7d5ee6b50
Mach Kobayashi <machwerx@gmail.com>
2023-01-08 07:34:37 -0800

    Implemented game stages

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,6 +10,16 @@ public class GameManager : MonoBehaviour {
 
     [SerializeField] private GameObject m_GameUiGroup;
 
+    [SerializeField] private TimeGraph m_PopulationGraph;
+    [SerializeField] private TimeGraph m_CarrotGraph;
+    [SerializeField] private TimeGraph m_ProductivityGraph;
+    [SerializeField] private TimeGraph m_CivilizationGraph;
+
+    [SerializeField] private TMPro.TextMeshPro m_PopulationLegend;
+    [SerializeField] private TMPro.TextMeshPro m_CarrotLegend;
+    [SerializeField] private TMPro.TextMeshPro m_ProductivityLegend;
+    [SerializeField] private TMPro.TextMeshPro m_CivilizationLegend;
+
     [SerializeField] private UiButton m_CarrotButton;
     [SerializeField] private UiButton m_MiningButton;
     [SerializeField] private UiButton m_ResearchButton;
@@ -17,11 +27,6 @@ public class GameManager : MonoBehaviour {
     [SerializeField] private UiSlider m_CarrotSlider;
     [SerializeField] private UiSlider m_MiningSlider;
     [SerializeField] private UiSlider m_ResearchSlider;
-
-    [SerializeField] private TimeGraph m_PopulationGraph;
-    [SerializeField] private TimeGraph m_CarrotGraph;
-    [SerializeField] private TimeGraph m_ProductivityGraph;
-    [SerializeField] private TimeGraph m_CivilizationGraph;
 
     [SerializeField] private GameObject m_MenuGroup;
     [SerializeField] private UiButton m_StartButton;
@@ -51,7 +56,7 @@ public class GameManager : MonoBehaviour {
 
     const float kSunRotationSpeed = 36.0f;
     const float kPlanetRotationSpeed = 72.0f;
-    const float kMaxAge = 1000.0f;
+    const float kMaxAge = 10000.0f;
     const float kYearsPerSecond = 200.0f;
 
     enum GameMode {
@@ -96,10 +101,12 @@ public class GameManager : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
+        float gameDeltaTime = 2 * Time.deltaTime;
+
         // rotate sun and planet
-        m_SunRotation += Time.deltaTime * kSunRotationSpeed;
+        m_SunRotation += gameDeltaTime * kSunRotationSpeed;
         m_Sun.localRotation = Quaternion.Euler(0, m_SunRotation, 0);
-        m_PlanetRotation += Time.deltaTime * kPlanetRotationSpeed;
+        m_PlanetRotation += gameDeltaTime * kPlanetRotationSpeed;
         m_Planet.localRotation = Quaternion.Euler(0, m_PlanetRotation, 0);
 
         // check for elements being interacted with
@@ -128,18 +135,21 @@ public class GameManager : MonoBehaviour {
 
         } else if (m_GameMode == GameMode.PlayingGame) {
             // do simulation
-            m_GameAge += Time.deltaTime * kYearsPerSecond;
-            m_CarrotButton.value += Time.deltaTime;
-            m_MiningButton.value += 0.3f * Time.deltaTime;
-            m_ResearchButton.value += 0.5f * Time.deltaTime;
+            m_GameAge += gameDeltaTime * kYearsPerSecond;
+            if (m_GameAge > kMaxAge) {
+                m_GameAge = kMaxAge;
+            }
+            m_CarrotButton.value += gameDeltaTime;
+            m_MiningButton.value += 0.3f * gameDeltaTime;
+            m_ResearchButton.value += 0.5f * gameDeltaTime;
 
             // bunnies eat carrots
-            m_CarrotCount -= Time.deltaTime * m_Population;
+            m_CarrotCount -= gameDeltaTime * m_Population;
 
             // figure out reproduction factor
             float reproductionFactor = Mathf.Clamp01(m_CarrotCount / m_Population / 10.0f);
             reproductionFactor = 0.5f + 1.5f * reproductionFactor;
-            m_Population *= Mathf.Pow(reproductionFactor, Time.deltaTime);
+            m_Population *= Mathf.Pow(reproductionFactor, gameDeltaTime);
             m_Population = Mathf.Clamp(m_Population, 2.0f, 1e10f);
 
             // update graphs
@@ -164,6 +174,47 @@ public class GameManager : MonoBehaviour {
     void InitGame() {
         UpdateUpgradeButton();
 
+        m_ProductivityGraph.gameObject.SetActive(false);
+        m_CivilizationGraph.gameObject.SetActive(false);
+
+        m_ProductivityLegend.gameObject.SetActive(false);
+        m_CivilizationLegend.gameObject.SetActive(false);
+
+        m_CarrotButton.m_AutoPress = false;
+        m_CarrotSlider.transform.parent.gameObject.SetActive(false);
+        m_MiningButton.transform.parent.parent.parent.gameObject.SetActive(false);
+        m_MiningButton.m_AutoPress = false;
+        m_MiningSlider.transform.parent.gameObject.SetActive(false);
+        m_ResearchButton.transform.parent.parent.parent.gameObject.SetActive(false);
+        m_ResearchSlider.transform.parent.gameObject.SetActive(false);
+
+        if (m_UpgradeLevel == UpgradeLevel.NormalBunnies) {
+            // default UI
+        } else if (m_UpgradeLevel == UpgradeLevel.SmarterBunnies) {
+            m_MiningButton.transform.parent.parent.parent.gameObject.SetActive(true);
+        } else if (m_UpgradeLevel == UpgradeLevel.BunnyAutonomy) {
+            m_ProductivityGraph.gameObject.SetActive(true);
+            m_ProductivityLegend.gameObject.SetActive(true);
+            m_CarrotButton.m_AutoPress = true;
+            m_CarrotSlider.transform.parent.gameObject.SetActive(true);
+            m_MiningButton.transform.parent.parent.parent.gameObject.SetActive(true);
+            m_MiningButton.m_AutoPress = true;
+            m_MiningSlider.transform.parent.gameObject.SetActive(true);
+        } else {
+            m_ProductivityGraph.gameObject.SetActive(true);
+            m_ProductivityLegend.gameObject.SetActive(true);
+            m_CarrotButton.m_AutoPress = true;
+            m_CivilizationGraph.gameObject.SetActive(true);
+            m_CivilizationLegend.gameObject.SetActive(true);
+            m_CarrotSlider.transform.parent.gameObject.SetActive(true);
+            m_MiningButton.transform.parent.parent.parent.gameObject.SetActive(true);
+            m_MiningButton.m_AutoPress = true;
+            m_MiningSlider.transform.parent.gameObject.SetActive(true);
+            m_ResearchButton.transform.parent.parent.parent.gameObject.SetActive(true);
+            m_ResearchSlider.transform.parent.gameObject.SetActive(true);
+        }
+
+
         m_SunRotation = 0;
         m_PlanetRotation = 0;
 
@@ -173,17 +224,17 @@ public class GameManager : MonoBehaviour {
         m_Population = 2;
         m_PopulationGraph.Init(m_Population, 1e10f, true);
 
-        m_CarrotButton.m_AutoPress = true;
+        m_CarrotButton.value = 0;
         m_CarrotSlider.value = 0.5f;
-        m_CarrotCount = 100;
+        m_CarrotCount = 5;
         m_CarrotGraph.Init(m_CarrotCount, 1e10f, true);
 
-        m_MiningButton.m_AutoPress = true;
+        m_MiningButton.value = 0;
         m_MiningSlider.value = 0.5f;
         m_Productivity = 20;
         m_ProductivityGraph.Init(m_Productivity, 1e10f, true);
 
-        m_ResearchButton.m_AutoPress = true;
+        m_ResearchButton.value = 0;
         m_ResearchSlider.value = 0.0f;
         m_Civilization = 1;
         m_CivilizationGraph.Init(m_Civilization, 1e10f, true);
